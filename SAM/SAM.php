b737864d18f3a1ddf3309d862a8b50383427f66f
tanwir1703 <tanwir1703@users.noreply.github.com>
2020-10-17 13:43:17 +0530

    SAM and SAMP webpages.

---
+++ SAM/SAM.php
@@ -0,0 +1,483 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="mobile_style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS only -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+  <!-- JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+    crossorigin="anonymous"></script>
+
+  <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="img/sac_logo.png"> </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Events
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Students Alumni Meet</a>
+            <a class="dropdown-item" href="#">Alumni Meet</a>
+            <a class="dropdown-item" href="#">Homecomming</a>
+            <a class="dropdown-item" href="#">Phonathon</a>
+            <a class="dropdown-item" href="#">Leadership Summit</a>
+            <a class="dropdown-item" href="#">Alumni Talks</a>
+            <a class="dropdown-item" href="#">ALVIDA</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Initiatives
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">SAMP</a>
+            <a class="dropdown-item" href="#">SPARC</a>
+            <a class="dropdown-item" href="#">MYIMPRINT</a>
+            <a class="dropdown-item" href="#">ACAP</a>
+            <a class="dropdown-item" href="#">Alumni Blog</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Publications
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Yearbook</a>
+            <a class="dropdown-item" href="#">Yearnings of Yore</a>
+            <a class="dropdown-item" href="#">Tempo Shout</a>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">The Team</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Alumni-Reg</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Sponsors
+          </a>
+          <div class="dropdown-menu" style="padding-right: 50px !important;" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">2019</a>
+            <a class="dropdown-item" href="#">2018</a>
+            <a class="dropdown-item" href="#">2017</a>
+            <a class="dropdown-item" href="#">2016</a>
+            <a class="dropdown-item" href="#">2015</a>
+            <a class="dropdown-item" href="#">2014</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+
+
+  <header>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-7 col-sm-12  text-white">
+          <h1 style="text-align: left;">STUDENT</h1>
+          <h1 style="text-align: left;"> ALUMNI MEET</h1>
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+              aria-valuemax="100"></div>
+          </div>
+          <br>
+          <h6 style="text-align: left;">DELHI | MUMBAI | BANGALORE | HYDERABAD | KOLKATA</h6>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <section class="section-1">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-6 col-12">
+            <div class="panel text-left">
+              <h1>ABOUT THE MEET</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                  aria-valuemax="100"></div>
+              </div>
+              <p class="pt-4">
+                Student Alumni Meets bring together the students and the alumni under one roof and encourage interactive
+                dialogue. They play a very important role in strengthening the bond with alumni.The aim of these meets
+                is to connect the alumni and the students at a very personal level and ensure long term interaction and
+                strong bonding thus proving to be extremely beneficial to both the students as well as the alumni
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6 col-12">
+            <div class="pray">
+              <br><br><br><br>
+              <img src="img/sam1.jpg" alt="Pray" class="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-2">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12 col-12">
+            <div class="panel text-center">
+              <h1 class="x">SAM'20 KOLKATA</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                  aria-valuemax="100"></div>
+              </div>
+              <br>
+              <!-- Photo Grid -->
+              <div id="gallery_row" class="row">
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/Kolkata/sam1.jpg" style="width:100%">
+                  <img class="image" src="img/sam/Kolkata/sam6.jpg" style="width:100%">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/Kolkata/sam3.jpg" style="width:100%">
+                  <img class="image" src="img/sam/Kolkata/sam4.jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/Kolkata/sam2.jpg" style="width:100%">
+                  <img class="image" src="img/sam/Kolkata/sam5.jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/Kolkata/sam.jpg" style="width:100%;">
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section-2">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12 col-12">
+            <div class="panel text-center">
+              <h1 class="x">SAM'18 SINGAPORE</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                  aria-valuemax="100"></div>
+              </div>
+              <br>
+
+              <!-- Photo Grid -->
+              <div id="gallery_row" class="row">
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/singapore 18/singapore (8).jpg" style="width:100%">
+                  <img class="image" src="img/sam/singapore 18/singapore (2).jpg" style="width:100%">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/singapore 18/singapore (3).jpg" style="width:100%">
+                  <img class="image" src="img/sam/singapore 18/singapore (4).jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/singapore 18/singapore (6).jpg" style="width:100%">
+                  <img class="image" src="img/sam/singapore 18/singapore (7).jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/singapore 18/singapore (1).jpg" style="width:100%;">
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="section-2">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12 col-12">
+            <div class="panel text-center">
+              <h1 class="x">SAM'18 BANGALORE</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                  aria-valuemax="100"></div>
+              </div>
+              <br>
+              <!-- Photo Grid -->
+              <div id="gallery_row" class="row">
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/bang18/1.jpg" style="width:100%">
+                  <img class="image" src="img/sam/bang18/2.jpg" style="width:100%">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/bang18/3.jpg" style="width:100%">
+                  <img class="image" src="img/sam/bang18/4.jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/bang18/5.jpg" style="width:100%">
+                  <img class="image" src="img/sam/bang18/6.jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/bangalore18.jpg" style="width:100%;">
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="section-2">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12 col-12">
+            <div class="panel text-center">
+              <h1 class="x">SAM'17 HYDERABAD</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                  aria-valuemax="100"></div>
+              </div>
+              <br>
+              <!-- Photo Grid -->
+              <div id="gallery_row" class="row">
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/hyderabad17/1.jpg" style="width:100%">
+                  <img class="image" src="img/sam/hyderabad17/2.jpg" style="width:100%">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/hyderabad17/3.jpg" style="width:100%">
+                  <img class="image" src="img/sam/hyderabad17/4.jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/hyderabad17/5.jpg" style="width:100%">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/Kolkata/sam.jpg" style="width:100%;">
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="section-2">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12 col-12">
+            <div class="panel text-center">
+              <h1 class="x">SAM'17 KOLKATA</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                  aria-valuemax="100"></div>
+              </div>
+              <br>
+              <!-- Photo Grid -->
+              <div id="gallery_row" class="row">
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/kolkata17/1.jpg" style="width:100%">
+                  <img class="image" src="img/sam/kolkata17/2.jpg" style="width:100%">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/kolkata17/3.jpg" style="width:100%">
+                  <img class="image" src="img/sam/kolkata17/4.jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/kolkata17/5.jpg" style="width:100%">
+                  <img class="image" src="img/sam/kolkata17/6.jpg" style="width:100%;">
+                </div>
+                <div id="gallery_row" class="column">
+                  <img class="image" src="img/sam/kolkata17/7.jpg" style="width:100%">
+                  <img class="image" src="img/sam/kolkata17/8.jpg" style="width:100%">
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <!-- 		Footer start -->
+  <main>
+    <section id='footer'>
+      <div class='container-fluid'>
+        <div class='row'>
+          <div class='col-4 footer-top-left'>
+            <h1> Get Social <br>With Us<br><span class='dots'>...</span>
+            </h1>
+          </div>
+          <div class=col-2>
+          </div>
+          <div class='col-6 footer-top-right'>
+            <h5>Students' Alumni Cell, IIT Kharagpur</h5>
+            <p>Office of Alumni Affairs & International Relations,<br>
+              Indian Institute of Technology, Kharagpur<br>
+              Pin-721302, West Bengal, India<br><br>
+              <em>Ph: 03222-281860</em> <br>
+              <em>Email: aao@hijli.iitkgp.ernet.in</em>
+            </p>
+            <hr>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-2 '>
+            <span class='bar'>|</span>
+          </div>
+          <div class=col-2>
+            <i class="icon fab fa-facebook"></i>
+          </div>
+          <div class='col-2'>
+            <i class="icon fab fa-instagram"></i>
+          </div>
+          <div class='col-2'>
+            <i class="icon fab fa-twitter-square"></i>
+          </div>
+          <div class='col-2'>
+            <i class="icon fab fa-medium"></i>
+          </div>
+          <div class='col-2'>
+            <i class="icon fab fa-linkedin"></i>
+          </div>
+
+        </div>
+      </div>
+  </main>
+  <!-- 	Footer end	 -->
+
+  <!--    Copyright -->
+  </section>
+  <section id='copyright'>
+    <p>&copy; 2018 Students' Alumni Cell</p>
+  </section>
+  <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/gsap-latest-beta.min.js"></script>
+  <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/ScrollTrigger.min.js"></script>
+  <script>
+    gsap.from('header', { opacity: 0, duration: 1 })
+    gsap.from('header h1', { opacity: 0, duration: 2, x: -200 })
+    gsap.from('header h6', { opacity: 0, duration: 2, x: -500 })
+
+
+    gsap.from('.nav-item', {
+      scrollTrigger: {
+        trigger: '.nav-item',
+        toggleActions: "play none none none"
+      },
+      opacity: 0,
+      duration: 1,
+      x: -200
+    });
+
+
+    gsap.from('.section-1 h1', {
+      scrollTrigger: {
+        trigger: '.section-1 h1',
+        toggleActions: "play none none none"
+      },
+      opacity: 0,
+      duration: 1,
+      x: -200
+    });
+
+    gsap.from('.section-1 p', {
+      scrollTrigger: {
+        trigger: '.section-1 p',
+        toggleActions: "play none none none"
+      },
+      opacity: 0,
+      duration: 2.0,
+    });
+
+    gsap.from('.section-1 img', {
+      scrollTrigger: {
+        trigger: '.section-1 img',
+        toggleActions: "play none none none"
+      },
+      opacity: 0,
+      duration: 1.5,
+      x: 200
+    });
+
+    var arr = document.getElementsByClassName("x");
+    for (var i = 0; i < arr.length; i++) {
+
+      gsap.from(arr[i], {
+        scrollTrigger: {
+          trigger: arr[i],
+          toggleActions: "play none none none"
+        },
+        opacity: 0,
+        duration: 1.5,
+        x: -200
+      });
+
+    }
+    var img_arr = document.getElementsByClassName("image");
+    for (var i = 0; i < img_arr.length; i++) {
+
+      gsap.from(img_arr[i], {
+        scrollTrigger: {
+          trigger: img_arr[i],
+          toggleActions: "play none none none",
+          start: "top center"
+        },
+        opacity: 0,
+        duration: 1.5,
+      });
+
+    }
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('#panel').forEach((panel, i) => {
+      ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        pin: true,
+        pinSpacing: false
+      });
+    });
+  </script>
+</body>
+
+</html>
