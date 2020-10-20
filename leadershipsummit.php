55f3924fa95b1f4f660bb1850f4f9728db7545f8
AryanLohani <64068225+AryanLohani@users.noreply.github.com>
2020-10-20 14:04:48 +0530

    leadership summit changes

---
+++ leadershipsummit.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/leadership.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS only -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <!-- JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+  <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="img/SACLogo.png"> </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Events
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Students Alumni Meet</a>
+            <a class="dropdown-item" href="#">Alumni Meet</a>
+            <a class="dropdown-item" href="#">Homecomming</a>
+            <a class="dropdown-item" href="#">Phonathon</a>
+            <a class="dropdown-item" href="#">Leadership Summit</a>
+            <a class="dropdown-item" href="#">Alumni Talks</a>
+            <a class="dropdown-item" href="#">ALVIDA</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Initiatives
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">SAMP</a>
+            <a class="dropdown-item" href="#">SPARC</a>
+            <a class="dropdown-item" href="#">MYIMPRINT</a>
+            <a class="dropdown-item" href="#">ACAP</a>
+            <a class="dropdown-item" href="#">Alumni Blog</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Publications
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Yearbook</a>
+            <a class="dropdown-item" href="#">Yearnings of Yore</a>
+            <a class="dropdown-item" href="#">Tempo Shout</a>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">The Team</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Alumni-Reg</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Sponsors
+          </a>
+          <div class="dropdown-menu" style="padding-right: 50px !important;" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">2019</a>
+            <a class="dropdown-item" href="#">2018</a>
+            <a class="dropdown-item" href="#">2017</a>
+            <a class="dropdown-item" href="#">2016</a>
+            <a class="dropdown-item" href="#">2015</a>
+            <a class="dropdown-item" href="#">2014</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+
+
+  <header>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-7 col-sm-12  text-white">
+          <h1 style="text-align: left;">Leadership</h1>
+          <h1 style="text-align: left;"> Summit</h1>
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <section class="section-1">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-6 col-12">
+            <div class="panel text-left">
+              <h1>The Summit</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+              <p class="pt-4">
+                <strong>Leadership Summit</strong>, organized
+                as a part of Guest Lectures, sees the participation of professionals and corporate tycoons sharing the same stage talking about their experiences and guiding the students to become future world leaders. With IIT Kharagpur’s
+                best minds converging at the this event, we weave together complex, seemingly diverse thoughts into an intricate tapestry and
+                provide a platform for them to share their experiences and guide students to become future world leaders.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6 col-12">
+            <div class="pray">
+              <br><br><br><br>
+              <img src="img/sam/sam_about.jpg" alt="Pray" class="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section-2">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12 col-12">
+            <div class="panel text-center">
+              <h1 class="x">Past Speakers</h1>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+              <br>
+              <!-- Photo Grid -->
+              <div id="gallery_row" class="row">
+                <div class="row justify-content-center px-4">
+                  <div class="col-md-4 col-sm-6 col-xs-12 py-4 image">
+                    <div class="card">
+                      <img src="img\leadership_speakers\arjunmalhotra.jpg" class="card-img">
+                      <div class="card-title">
+                        <p><a>Arjun Malhotra</a></p>
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-sm-6 col-xs-12 py-4  image">
+                    <div class="card">
+                      <img src="img\leadership_speakers\anjanraichaudhuri.jpg" class="card-img">
+                      <div class="card-title">
+                        <p><a>Anjanrai Chaudhuri</a></p>
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-sm-6 col-xs-12 py-4  image">
+                    <div class="card">
+                      <img src="img\leadership_speakers\arunabhkumar.jpg" class="card-img">
+                      <div class="card-title">
+                        <p>Arunabh Kumar</p>
+
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-sm-6 col-xs-12 py-4  image">
+                    <div class="card">
+                      <img src="img\leadership_speakers\gopalrajgarhia.jpg" class="card-img">
+                      <div class="card-title">
+                        <p>Gopalraj Garhia</p>
+
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-sm-6 col-xs-12 py-4  image">
+                    <div class="card">
+                      <img src="img\leadership_speakers\prasadrmenon.jpg" class="card-img">
+                      <div class="card-title">
+                        <p>Prasad Menon</p>
+
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-sm-6 col-xs-12 py-4  image">
+                    <div class="card">
+                      <img src="img\leadership_speakers\sakyasinharoy.jpg" class="card-img">
+                      <div class="card-title">
+                        <p>Sakyasinha Roy</p>
+
+
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+    </section>
+
+
+    <!-- 		Footer start -->
+    <main>
+      <section id='footer'>
+        <div class='container-fluid'>
+          <div class='row'>
+            <div class='col-4 footer-top-left'>
+              <h1> Get Social <br>With Us<br><span class='dots'>...</span>
+              </h1>
+            </div>
+            <div class=col-2>
+            </div>
+            <div class='col-6 footer-top-right'>
+              <h5>Students' Alumni Cell, IIT Kharagpur</h5>
+              <p>Office of Alumni Affairs & International Relations,<br>
+                Indian Institute of Technology, Kharagpur<br>
+                Pin-721302, West Bengal, India<br><br>
+                <em>Ph: 03222-281860</em> <br>
+                <em>Email: aao@hijli.iitkgp.ernet.in</em>
+              </p>
+              <hr>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='col-2 '>
+              <span class='bar'>|</span>
+            </div>
+            <div class=col-2>
+              <i class="icon fab fa-facebook"></i>
+            </div>
+            <div class='col-2'>
+              <i class="icon fab fa-instagram"></i>
+            </div>
+            <div class='col-2'>
+              <i class="icon fab fa-twitter-square"></i>
+            </div>
+            <div class='col-2'>
+              <i class="icon fab fa-medium"></i>
+            </div>
+            <div class='col-2'>
+              <i class="icon fab fa-linkedin"></i>
+            </div>
+
+          </div>
+        </div>
+    </main>
+    <!-- 	Footer end	 -->
+
+    <!--    Copyright -->
+    </section>
+    <section id='copyright'>
+      <p>&copy; 2018 Students' Alumni Cell</p>
+    </section>
+    <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/gsap-latest-beta.min.js"></script>
+    <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/ScrollTrigger.min.js"></script>
+    <script>
+      gsap.from('header', {
+        opacity: 0,
+        duration: 1
+      })
+      gsap.from('header h1', {
+        opacity: 0,
+        duration: 2,
+        x: -200
+      })
+      gsap.from('header h6', {
+        opacity: 0,
+        duration: 2,
+        x: -500
+      })
+
+
+      gsap.from('.nav-item', {
+        scrollTrigger: {
+          trigger: '.nav-item',
+          toggleActions: "play none none none"
+        },
+        opacity: 0,
+        duration: 1,
+        x: -200
+      });
+
+
+      gsap.from('.section-1 h1', {
+        scrollTrigger: {
+          trigger: '.section-1 h1',
+          toggleActions: "play none none none"
+        },
+        opacity: 0,
+        duration: 1,
+        x: -200
+      });
+
+      gsap.from('.section-1 p', {
+        scrollTrigger: {
+          trigger: '.section-1 p',
+          toggleActions: "play none none none"
+        },
+        opacity: 0,
+        duration: 2.0,
+      });
+
+      gsap.from('.section-1 img', {
+        scrollTrigger: {
+          trigger: '.section-1 img',
+          toggleActions: "play none none none"
+        },
+        opacity: 0,
+        duration: 1.5,
+        x: 200
+      });
+
+      var arr = document.getElementsByClassName("x");
+      for (var i = 0; i < arr.length; i++) {
+
+        gsap.from(arr[i], {
+          scrollTrigger: {
+            trigger: arr[i],
+            toggleActions: "play none none none"
+          },
+          opacity: 0,
+          duration: 1.5,
+          x: -200
+        });
+
+      }
+      var img_arr = document.getElementsByClassName("image");
+      for (var i = 0; i < img_arr.length; i++) {
+
+        gsap.from(img_arr[i], {
+          scrollTrigger: {
+            trigger: img_arr[i],
+            toggleActions: "play none none none",
+            start: "top center"
+          },
+          opacity: 0,
+          duration: 1.5,
+        });
+
+      }
+
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.utils.toArray('#panel').forEach((panel, i) => {
+        ScrollTrigger.create({
+          trigger: panel,
+          start: "top top",
+          pin: true,
+          pinSpacing: false
+        });
+      });
+    </script>
+</body>
+
+</html>
---
+++ yearbook/yearbook.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+        .heading {
+            background-image: url("imgs/1st page-01.jpg");
+            height: 100vh;
+            background-position: center bottom;
+            background-repeat: no-repeat;
+            background-size: cover;
+            font-size: 4.5em;
+            color: white;
+        }
+
+        .gradient-1 {
+            height: 20vh;
+            background: linear-gradient(#b3653c, #2b1a45);
+        }
+
+        .content {
+            background-image: url("imgs/night-01.jpg");
+            height: 100vh;
+            background-position: center top;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        .content-text{
+            color: white;
+            border-radius: 10px;
+            background-color: rgba(100,149,237,0.2);
+            margin-top: 10%;
+            box-shadow: 10px 10px 5px rgba(0,0,0,0.4);
+        }
+        .content-text h1{
+            color:rgb(25,25,112);
+            font-size: 3rem;
+        }
+        .content-text h4{
+            font-size: 1.3em;
+        }
+        .yearbooks {
+            background-image: url("imgs/bricks_Mesa de trabajo 1.jpg");
+            height: 200vh;
+            background-position: center bottom;
+            background-repeat: no-repeat;
+            background-size: cover;
+            font-size: 7rem;
+            color: white;
+        }
+        .book{
+            height: 400px;
+            width: 100%;
+            max-width: 300px;
+            object-fit: cover;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="heading ">
+        <div class="container-fluid" style="max-width:1400px">
+            <div class="row" style="height: 100vh;">
+                <div class="col-md-6 px-6 align-self-center">
+                    Yearbook<br/>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="gradient-1"></section>
+    <section class="content ">
+        <div class="container-fluid" style="max-width:1400px">
+            <div class="row px-md-5 px-2">
+                <div class="col-md-6 col-x-12 py-4 content-text">
+                    <div class="px-3 py-1">
+                    <h1 class="">About</h1>
+                     <div class="progress ">
+                        <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100"
+                            aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <h4 class="">The yearbook is a dedicated volume that captures the nostalgic memories of the graduating batch of our Institute. The beautiful years spent in the Institute are engraved and expressed via photos and writeups which is given as a souvenir of departure from the Institute. Few years down the road when they will walk down the memory lane, 
+                    the yearbook will be the light which will lit up the faded memories and show the path through the old forgotten ways
+                    </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="yearbooks">
+        <div class="container-fluid">
+            <div class="row-xs-12 py-4" style="text-align: center;">
+                <div class="col">
+                    <p style="font-size: 50px;">The Yearbooks</p>
+                </div>
+            </div>
+            <div class="row-xs-12 py-4">
+                <div class="col-md-6 col-xs-12 px-5 ">
+                    <img src="imgs\yearbook\year2012.jpg" class="book">
+                </div>
+            </div>
+            <div class="row-xs-12 py-4">
+            <div class="col-md-6 col-xs-12">
+                    
+                    </div>
+                <div class="col-md-6 col-xs-12">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
