395b38e1bf544b27a37fbfa1eef7055842433cfa
tanwir1703 <tanwir1703@users.noreply.github.com>
2020-10-23 21:29:04 +0530

    footer
    Created footer.php and included in SAM, SAMP, YOY.

---
+++ footer.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Students' Alumni Cell, IIT Kharagpur</title>
+
+    <!-- Bootstrap Scripts CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Bootstrap scripts Jquery -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+    <style>
+    * {
+        margin: 0;
+        padding: 0;
+
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    body {
+        width: 100%;
+    }
+
+    #footer {
+        background-color: #23232b;
+        height: 18rem;
+    }
+
+    .footer-top-left {
+        height: 12rem;
+        background-image: linear-gradient(to bottom right, #283949, #342b2d);
+        color: white;
+        padding-top: 1.8rem;
+    }
+
+    .footer-top-right {
+        padding: 1.8rem;
+        color: #eef0ef;
+
+    }
+
+    .dots {
+        font-size: 2em;
+        position: relative;
+        bottom: 2.5rem;
+    }
+
+    .bar {
+        font-size: 3em;
+        color: white;
+        position: absolute;
+        top: -2.8rem;
+        left: 2rem;
+
+
+    }
+
+    .icon {
+        font-size: 2em;
+        color: white;
+        position: absolute;
+        top: -1rem;
+
+    }
+
+    #copyright {
+        background-color: black;
+        text-align: center;
+        color: grey;
+    }
+    </style>
+
+</head>
+
+<body>
+
+
+    <section id='footer'>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-4 footer-top-left'>
+                    <h1> Get Social <br>With Us<br><span class='dots'>...</span>
+                    </h1>
+                </div>
+                <div class=col-2>
+                </div>
+                <div class='col-6 footer-top-right'>
+                    <h5>Students' Alumni Cell, IIT Kharagpur</h5>
+                    <p>Office of Alumni Affairs & International Relations,<br>
+                        Indian Institute of Technology, Kharagpur<br>
+                        Pin-721302, West Bengal, India<br><br>
+                        <em>Ph: 03222-281860</em> <br>
+                        <em>Email: aao@hijli.iitkgp.ernet.in</em>
+                    </p>
+                    <hr>
+                </div>
+            </div>
+            <div class='row'>
+                <div class='col-2 '>
+                    <span class='bar'>|</span>
+                </div>
+                <div class=col-2>
+                    <i class="icon fab fa-facebook"></i>
+                </div>
+                <div class='col-2'>
+                    <i class="icon fab fa-instagram"></i>
+                </div>
+                <div class='col-2'>
+                    <i class="icon fab fa-twitter-square"></i>
+                </div>
+                <div class='col-2'>
+                    <i class="icon fab fa-medium"></i>
+                </div>
+                <div class='col-2'>
+                    <i class="icon fab fa-linkedin"></i>
+                </div>
+
+            </div>
+        </div>
+        <!-- 	Footer end	 -->
+		
+<!--    Copyright -->
+  	</section>
+  	<section id='copyright'>
+  		<p>&copy; 2018 Students' Alumni Cell</p>
+  	</section>
+  	
+
+  </body>
+</html>
