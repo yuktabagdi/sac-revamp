3d4a6113918de5a4b1c4df225c4e7511e5b972e1
AryanLohani <64068225+AryanLohani@users.noreply.github.com>
2020-10-29 15:10:35 +0530

    yearbook carousel added

--- yearbook.php
+++ yearbook.php
@@ -22,16 +22,13 @@
 
 <body>
 
-        
-<?php include 'navbar.php' ?>
+
+    <?php include 'navbar.php' ?>
     <header>
         <div class="container text-center">
             <div class="row">
                 <div class="col-md-7 col-sm-12  text-white">
-                    <h1 style="text-align: left;"> YearBook</h1>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
+                    
                 </div>
             </div>
         </div>
@@ -70,14 +67,14 @@
         </section>
 
 
-        <section class="section-2 content-box py-5 px-md-5">
+        <!-- <section class="section-2 content-box py-5 px-md-5">
             <div class="container-fluid ">
                 <div class="row-xs-12 text-center">
                     <div class="col headin">
                         The YearBooks
                         <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
+                            <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
                     </div>
                 </div>
                 <div class="row py-2 justify-content-center">
@@ -171,7 +168,163 @@
 
                 </div>
             </div>
-            
+
+        </section> -->
+        <section class="section-3">
+            <div class="container-fluid">
+                <h1 class="text-center">Exibition Corner......</h1>
+                <div class="row">
+                    <div class="col-lg-2"></div>
+                    <div class="col-sm-12 col-lg-8">
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div id="inam" class="carousel slide" data-ride="carousel" data-interval="false">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-sm-12 col-lg-4">
+                                                <div class="card" style="height: 100% !important;">
+                                                    <img src="img/yoy/yoy188.jpg" class="card-img-top">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">YearBook2017....</h4>
+                                                        <div class="row justify-content-around py-3">
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>UG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>PG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>RS</h4>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+                                            <div class="col-sm-12 col-lg-4">
+                                                <div class="card" style="height: 100% !important;">
+                                                    <img src="img/yoy/yoy17.jpg" class="card-img-top">
+                                                    <div class="card-body">
+                                                    <h4 class="card-title">YearBook2016....</h4>
+                                                        <div class="row justify-content-around py-3">
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>UG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>PG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>RS</h4>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+                                            <div class="col-sm-12 col-lg-4">
+                                                <div class="card" style="height: 100% !important;">
+                                                    <img src="img/yoy/2016.jpg" class="card-img-top">
+                                                    <div class="card-body">
+                                                    <h4 class="card-title">YearBook2015....</h4>
+                                                        <div class="row justify-content-around py-3">
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>UG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>PG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>RS</h4>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="container">
+                                        <div class="row justify-content-center" >
+                                            <div class="col-sm-12 col-lg-4">
+                                                <div class="card" style="height: 100% !important;">
+                                                    <img src="img/yoy/2015.jpg" class="card-img-top">
+                                                    <div class="card-body">
+                                                    <h4 class="card-title">YearBook2014....</h4>
+                                                        <div class="row justify-content-around py-3">
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>UG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>PG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>RS</h4>
+                                                            </div>
+                                                        </div>
+
+
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+                                            <div class="col-sm-12 col-lg-4">
+                                                <div class="card" style="height: 100% !important;">
+                                                    <img src="img/yoy/2014.jpg" class="card-img-top">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">YearBook2013....</h4>
+                                                        <div class="row justify-content-around py-3">
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>UG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>PG</h4>
+                                                            </div>
+                                                            <div class="col px-1 text-center img-txt">
+                                                                <h4>RS</h4>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                                <a href="#inam" class="carousel-control-prev" data-slide="prev">
+                                    <span class="carousel-control-prev-icon bg-dark"></span>
+                                </a>
+                                <a href="#inam" class="carousel-control-next" data-slide="next">
+                                    <span class="carousel-control-next-icon bg-dark"></span>
+                                </a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
         </section>
         <section style="height: 100px;"></section>
         <!-- 		Footer start -->
@@ -237,12 +390,12 @@
             });
 
             var img_txt = document.getElementsByClassName("img-txt");
-            var book = document.getElementsByClassName("book");
+            var card = document.getElementsByClassName("card");
 
-            for (let i = 0; i < book.length; i++) {
-                gsap.from(book[i], {
+            for (let i = 0; i < card.length; i++) {
+                gsap.from(card[i], {
                     scrollTrigger: {
-                        trigger: book[i],
+                        trigger: card[i],
                         toggleActions: "play none none none"
                     },
                     opacity: 0,
@@ -251,7 +404,7 @@
                 });
 
 
-                $(book[i]).on('mouseenter', function(e) {
+                $(card[i]).on('mouseenter', function(e) {
                     for (let j = 0; j < 3; j++) {
                         gsap.to($(img_txt[i * 3 + j]), 1, {
                             opacity: 1,
@@ -259,7 +412,7 @@
                         });
                     }
                 });
-                $(book[i]).on('mouseleave', function(e) {
+                $(card[i]).on('mouseleave', function(e) {
                     for (let j = 0; j < 3; j++) {
                         gsap.to($(img_txt[i * 3 + j]), 1, {
                             opacity: 0,
