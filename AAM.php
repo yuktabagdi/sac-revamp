6d31d8244fb1dfcaeb0b3f7f2d9c4adf9969d851
vedica24 <64041087+vedica24@users.noreply.github.com>
2020-11-09 15:26:21 +0530

    AAM

--- AAM.php
+++ AAM.php
@@ -12,7 +12,7 @@
 
 
 <body style="background:#fcf6f4;">
-  <section class="section-1">
+  <section class="section-1 pb-0">
       <?php include'navbar.php' ?>
     <header>
     <div class="header">
@@ -21,39 +21,45 @@
 	</div>
 	</header>
   </section>
-  
-    <section class="section-2" style="padding-bottom:10rem;">
-    <div class="container-fluid text-center d-flex align-items-center">
-      <div class="row">
-        <div class="col-md-6 col-12 pt-0 pt-sm-5 about">
-          <div class="panel pt-3 pt-sm-5 text-left">
-            <h1 class="one">ABOUT US</h1>
-              
-            <p class="pt-3 one">
+  <section class="section-2 pt-0" style="padding-bottom:10rem;">
+	<div class="container-fluid" style="padding:0 5rem;">
+		<div class="row">
+			<div class="col-md-6">
+				<h1 class="one">ABOUT US</h1>
+				<div class="row">
+					<div class="col-md-10 justify-content-left">
+				<p class="pt-3 one">
 				We live our day to day lives oblivious to the beauty that surrounds us and by the time we do realise what
 				exactly it is that we've been blessed with, it's generally too late. However when the good ol' days call
 				yet again, nostalgia surrounds us as we walk the hallowed boulevards of our youth. The Annual Alumni Meet 
 				brings to our alumni a chance to reconnect with their Alma Mater. It lets them relive the carefree days of 
 				their college lives as they get a chance to visit all the locations they once frequented.
             </p>
-          </div>
-        </div>
-        <div class="col-md-6 col-12  pr-4  trailer">
-			<div class="first"></div>
-			<div class="second"></div>
-			<div class="third">
-			<iframe src="https://www.youtube.com/embed/BPaRgF4rTRE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>	
-          </div>
-        </div>
-      </div>
-  </section>
-  <section class="section-3 pb-5">
+			</div>
+			</div>
+			</div>
+			<div class="col-md-6">
+			<div class="container">
+				<div class="row d-flex justify-content-center">
+					<div class="col-md-10 col-12 trailer" style="padding-top:3.5rem; padding-bottom:15rem;">
+						<div class="first"></div>
+						<div class="second"></div>
+						<div class="third">
+							<iframe src="https://www.youtube.com/embed/BPaRgF4rTRE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>	
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>  
+    <section class="section-3 pb-5 pt-5">
 	<div class="container-fluid p-0">
 	<div style="padding:0 5rem;">
 		<div class="row">
 			<div class="col-md-3 text-center">
-				<h2 class="gallery-h3 pt-5">GRAND REUNION</h2>
+				<h1 class="gallery-h3 pt-5">DAY 1</h1>
 				<div class="box d-flex justify-content-center"></div>
 			</div>
 			<div class="col-md-9">
@@ -61,33 +67,166 @@
 				<div class="row d-flex justify-content-center">
 					<div class="col-md-7 x">
 					<div class="image">
-					<img  src="img/kgp_arena.jpg" class="img-fluid image__img">
-				      <div class="image__overlay image__overlay--primary">
-							<div class="image__title">title</div>
-							<p class="image__description">This is a short description</p>	
+					<img  src="img/day1/arena3.jpg" class="img-fluid image__img">
+				      <div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Arena</div>
+							<p class="image__description">The Arena is a nostalgia-filled site welcoming the Alumni to share their old memories with us.</p>	
 				</div></div>
 			</div>
 			</div>
 			<div class="row d-flex justify-content-center pt-4">
-					<div class="col-md-6 x">
+					<div class="col-md-8 x">
 					<div class="image">
-					<img  src="img/arena.jpg" class="img-fluid image__img">
+					<img  src="img/day1/arena2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
 				</div></div>
 				</div>
-				<div class="col-md-6 content-overlay x">
+				<div class="col-md-4 content-overlay x">
 									<div class="image">
 
-					<img  src="img/arena2.jpg" class="img-fluid image__img">
+					<img  src="img/day1/alumni.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
 				</div></div>
 				</div>
 			</div>
+			<div class="row d-flex justify-content-center pt-4 ">
+					<div class="col-md-4 content-overlay x">
+										<div class="image">
+
+					<img  src="img/day1/dept_visit1.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">title</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+				<div class="col-md-8 content-overlay pt-sm-0 pt-4 x">
+									<div class="image">
+
+					<img  src="img/day1/dept_visit2.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Department Visit</div>
+							<p class="image__description">Departments are our own shrine of knowledge filled with bittersweet 
+							memories, and a visit back to this shrine surely brings back memories of all the hard work, 
+							learning, and excitement.</p>	
+				</div></div>
+				</div>
+			</div>
+			<div class="row d-flex justify-content-center pt-4 x">
+					<div class="col-md-9 content-overlay">
+										<div class="image">
+
+					<img src="img/day1/akash2.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">Stand-Up Comedy</div>
+							<p class="image__description">Smiles can cast the most powerful spell of love and happiness.</p>	
+				</div></div>
+				</div>
+			
+			</div>
+			<div class="row d-flex justify-content-center pt-4 x">
+					<div class="col-md-8 content-overlay">
+										<div class="image">
+
+					<img  src="img/day1/toat.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">Entertania</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+				<div class="col-md-4 content-overlay pt-4 pt-sm-0 x">
+									<div class="image">
+
+					<img  src="img/day1/toat2.jpg" class="img-fluid image__img"><div class="image__overlay image__overlay--primary">
+							<div class="image__title">title</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+			</div>
 			<div class="row d-flex justify-content-center pt-4">
+					<div class="col-md-7 content-overlay x">
+										<div class="image">
+
+					<img src="img/day1/flames2.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">Hall Visit</div>
+							<p class="image__description">Visiting the place which was our second home back again.</p>	
+				</div></div>
+				</div>
+			
+			</div>
+			<div class="row d-flex justify-content-center pt-4">
+					<div class="col-md-4 content-overlay x">				
+					<div class="image">
+
+					<img  src="img/day1/flames.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">title</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+				<div class="col-md-8 content-overlay pt-4 pt-sm-0 x">
+									<div class="image">
+
+					<img  src="img/day1/hall_visit2.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Hall Visit</div>
+							<p class="image__description">The place we learnt and lived for the 4 most important years 
+							in our life surely has a fragment of our soul inside it and to come back to our hall 
+							of residence is nothing less than a reunion of all of our soul.
+</p>	
+				</div></div>
+				</div>
+			</div>
+			</div>
+		</div>
+		</div>
+	</div>
+	</div>
+	</div>	
+  </section>
+  <section class="section-3 pb-5 pt-5">
+	<div class="container-fluid p-0">
+	<div style="padding:0 5rem;">
+		<div class="row">
+			<div class="col-md-3 text-center">
+				<h1 class="gallery-h3 pt-5">DAY 2</h1>
+				<div class="box d-flex justify-content-center"></div>
+			</div>
+			<div class="col-md-9">
+			<div class="container">
+				<div class="row d-flex justify-content-center">
+					<div class="col-md-7 x">
+					<div class="image">
+					<img  src="img/day2/arena.jpg" class="img-fluid image__img">
+				      <div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Arena</div>
+							<p class="image__description">The Arena is a nostalgia-filled site welcoming the Alumni to share their old memories with us.</p>	
+				</div></div>
+			</div>
+			</div>
+			<div class="row d-flex justify-content-center pt-4">
+					<div class="col-md-8 x">
+					<div class="image">
+					<img  src="img/day2/nalanda.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Campus Tour</div>
+							<p class="image__description">Campus Tour is our way of showing the 2200 acre campus in a nutshell and highlighting all the major developments around.
+</p>	
+				</div></div>
+				</div>
+				<div class="col-md-4 content-overlay x">
+									<div class="image">
+
+					<img  src="img/day2/nalanda2.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">	
+				</div></div>
+				</div>
+			</div>
+			<!--<div class="row d-flex justify-content-center pt-4">
 					<div class="col-md-8 content-overlay x">
 										<div class="image">
 
@@ -98,32 +237,34 @@
 				</div></div>
 				</div>
 			
-			</div>
+			</div>-->
+			
 			<div class="row d-flex justify-content-center pt-4 ">
-					<div class="col-md-4 content-overlay x">
-										<div class="image">
+					
+				<div class="col-md-4 content-overlay pt-sm-0 pt-4 x">
+									<div class="image">
 
-					<img  src="img/flames.jpg" class="img-fluid image__img">
+					<img  src="img/day2/campus_tour2.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
 				</div></div>
 				</div>
-				<div class="col-md-8 content-overlay pt-sm-0 pt-4 x">
-									<div class="image">
+				<div class="col-md-8 content-overlay x">
+										<div class="image">
 
-					<img  src="img/flames2.jpg" class="img-fluid image__img">
-					<div class="image__overlay image__overlay--primary">
-							<div class="image__title">title</div>
-							<p class="image__description">This is a short description</p>	
+					<img  src="img/day2/campus_tour.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Campus Tour</div>
+							<p class="image__description">Remember the first time you've been shown the history of our college-Nehru Museum</p>	
 				</div></div>
 				</div>
 			</div>
 			<div class="row d-flex justify-content-center pt-4 x">
-					<div class="col-md-9 content-overlay">
+					<div class="col-md-7 content-overlay">
 										<div class="image">
 
-					<img src="img/she_says2.jpg" class="img-fluid image__img">
+					<img src="img/day2/football.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
@@ -132,19 +273,19 @@
 			
 			</div>
 			<div class="row d-flex justify-content-center pt-4 x">
-					<div class="col-md-8 content-overlay">
+					<div class="col-md-6 content-overlay">
 										<div class="image">
 
-					<img  src="img/emo.jpg" class="img-fluid image__img">
+					<img  src="img/day2/toat.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
 				</div></div>
 				</div>
-				<div class="col-md-4 content-overlay pt-4 pt-sm-0 x">
+				<div class="col-md-6 content-overlay pt-4 pt-sm-0 x">
 									<div class="image">
 
-					<img  src="img/marathon.jpg" class="img-fluid image__img"><div class="image__overlay image__overlay--primary">
+					<img  src="img/day2/toat2.jpg" class="img-fluid image__img"><div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
 				</div></div>
@@ -154,39 +295,51 @@
 					<div class="col-md-7 content-overlay x">
 										<div class="image">
 
-					<img src="img/toat.jpg" class="img-fluid image__img">
-					<div class="image__overlay image__overlay--primary">
-							<div class="image__title">title</div>
-							<p class="image__description">This is a short description</p>	
+					<img src="img/day2/kgp_arena.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Illumination</div>
+							<p class="image__description">The Illumination with diyas followed by the release of sky lanterns 
+							creates an aura of happiness all around before we kick.</p>	
 				</div></div>
 				</div>
 			
 			</div>
 			<div class="row d-flex justify-content-center pt-4">
-					<div class="col-md-6 content-overlay x">				
+					<div class="col-md-4 content-overlay x">				
 					<div class="image">
 
-					<img  src="img/visit.jpg" class="img-fluid image__img">
+					<img  src="img/day2/illu1.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
 				</div></div>
 				</div>
-				<div class="col-md-6 content-overlay pt-4 pt-sm-0 x">
+				<div class="col-md-8 content-overlay pt-4 pt-sm-0 x">
 									<div class="image">
 
-					<img  src="img/visit2.jpg" class="img-fluid image__img">
-					<div class="image__overlay image__overlay--primary">
-							<div class="image__title">title</div>
-							<p class="image__description">This is a short description</p>	
+					<img  src="img/day2/illu2.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Illumination</div>
+							<p class="image__description">The Illumination with diyas followed by the release of sky lanterns 
+							creates an aura of happiness all around before we kick.</p>	
 				</div></div>
 				</div>
 			</div>
 			<div class="row d-flex justify-content-center pt-4">
-					<div class="col-md-8 content-overlay x">
+					<div class="col-md-6 content-overlay x">
 										<div class="image">
 
-					<img src="img/akash2.jpg" class="img-fluid image__img">
+					<img src="img/day2/cake_cutting.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">Grand Reunion</div>
+							<p class="image__description">The Annual Alumni Meet is a fun and fascinating trip down memory lane that allows our alumni to see old friends
+							again and remember places and stories that were almost forgotten.</p>	
+				</div></div>
+				</div>
+				<div class="col-md-6 content-overlay x">
+										<div class="image">
+
+					<img src="img/day2/cake.jpg" class="img-fluid image__img">
 					<div class="image__overlay image__overlay--primary">
 							<div class="image__title">title</div>
 							<p class="image__description">This is a short description</p>	
@@ -199,13 +352,77 @@
 		</div>
 	</div>
 	</div>
-		
-	
-	
-	
-	
+	</div>	
+  </section>
+  <section class="section-3 pb-5 pt-5">
+	<div class="container-fluid p-0">
+	<div style="padding:0 5rem;">
+		<div class="row">
+			<div class="col-md-3 text-center">
+				<h1 class="gallery-h3 pt-5">DAY 3</h1>
+				<div class="box d-flex justify-content-center"></div>
+			</div>
+			<div class="col-md-9">
+			<div class="container">
+			<div class="row d-flex justify-content-center pt-4 ">
+					<div class="col-md-4 content-overlay x">
+										<div class="image">
+
+					<img  src="img/day3/emo.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">title</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+				<div class="col-md-8 content-overlay pt-sm-0 pt-4 x">
+									<div class="image">
+
+					<img  src="img/day3/marathon.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary text-center">
+							<div class="image__title">2.2 Marathon</div>
+							<p class="image__description">The iconic 2.2 km marathon around the heart of the campus in which all 
+							students and alumni are invited to run together to foster the spirit of good health and harmony. </p>	
+				</div></div>
+				</div>
+			</div>
+			<div class="row d-flex justify-content-center pt-4 x">
+					<div class="col-md-9 content-overlay">
+										<div class="image">
+
+					<img src="img/day3/closing_ceremony.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">title</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+			
+			</div>
+			<div class="row d-flex justify-content-center pt-4 ">
+					<div class="col-md-6 content-overlay x">
+										<div class="image">
+
+					<img  src="img/day3/ceremony4.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">title</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+				<div class="col-md-6 content-overlay pt-sm-0 pt-4 x">
+									<div class="image">
+
+					<img  src="img/day3/ceremony2.jpg" class="img-fluid image__img">
+					<div class="image__overlay image__overlay--primary">
+							<div class="image__title">title</div>
+							<p class="image__description">This is a short description</p>	
+				</div></div>
+				</div>
+			</div>
+			</div>
+		</div>
+		</div>
 	</div>
-	
+	</div>
+	</div>	
   </section>
   <?php include'footer.php' ?>
   <?php include 'preloader.php' ?>
